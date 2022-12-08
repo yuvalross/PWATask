6ea297f87c544e78d77a060e9f4ebb8a24e3c173
yuvalross <82139891+yuvalross@users.noreply.github.com>
2022-12-08 11:23:12 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,60 @@
+﻿window.addEventListener("DOMContentLoaded", function () {
+
+    document.getElementById("find-me").addEventListener("click", geoFindMe);
+    //document.getElementById("shareBtn").addEventListener("click", share);
+    var shareData;
+    var mapLink;
+
+    function geoFindMe() {
+
+        const status = document.querySelector('#status');
+        mapLink = document.querySelector('#map-link');
+      
+        mapLink.href = '';
+        mapLink.textContent = '';
+      
+        function success(position) {
+          const latitude  = position.coords.latitude;
+          const longitude = position.coords.longitude;
+
+          status.textContent = '';
+          mapLink.href = `https://www.openstreetmap.org/#map=18/${latitude}/${longitude}`;
+          mapLink.textContent = `Latitude: ${latitude} °, Longitude: ${longitude} °`;
+          document.getElementById("iframe").classList.remove('d-none');
+          document.getElementById("iframe").src=`https://maps.google.com/?output=embed&q=${latitude},${longitude}`;
+
+     
+        }
+
+        
+      
+        function error() {
+          status.textContent = 'Unable to retrieve your location';
+        }
+      
+        if (!navigator.geolocation) {
+          status.textContent = 'Geolocation is not supported by your browser';
+        } else {
+          status.textContent = 'Locating…';
+          navigator.geolocation.getCurrentPosition(success, error);
+        }
+      
+      }
+      
+      // Share must be triggered by "user activation"
+      document.getElementById("shareBtn").addEventListener('click', async () => {
+        shareData = {
+            title: 'MDN',
+            text: mapLink.textContent,
+            url:   mapLink.href
+          }
+
+        try {
+          await navigator.share(shareData);
+         // resultPara.textContent = 'MDN shared successfully';
+        } catch (err) {
+          //resultPara.textContent = `Error: ${err}`;
+        }
+      });
+      
+})
